fe7200d3dd79055e8b0ddcb20457dd75400b2605
sachin <sachin@sachin-ThinkPad-E470>
2022-11-14 21:19:19 +0530

    added mymoney app changes

--- src/mymoney/model/MonthlyMap.java
+++ src/mymoney/model/MonthlyMap.java
@@ -1,9 +1,10 @@
 package mymoney.model;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class MonthlyMap {
-	private static List<String> list = List.of("JANUARY", "FEBRUARY", "MARCH","APRIL", "MAY",
+	private static List<String> list = Arrays.asList("JANUARY", "FEBRUARY", "MARCH","APRIL", "MAY",
 			"JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER");
 	
 	public static int getMonthIndex(String month) {
